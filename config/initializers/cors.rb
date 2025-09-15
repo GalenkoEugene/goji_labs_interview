8a05eb2fcd5d90eacfd70c181ea249192bf3dde7
Yevhenii Halenko <Re4port@ukr.net>
2025-09-15 17:47:24 +0300

    Fix linter

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,9 +7,9 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*' # or a specific origin like 'http://localhost:3000'
-    resource '*',
+    origins "*" # or a specific origin like 'http://localhost:3000'
+    resource "*",
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      methods: [ :get, :post, :put, :patch, :delete, :options, :head ]
   end
 end
--- db/migrate/20250915143738_add_unique_index_to_enrollments.rb
+++ db/migrate/20250915143738_add_unique_index_to_enrollments.rb
@@ -1,6 +1,6 @@
 class AddUniqueIndexToEnrollments < ActiveRecord::Migration[8.0]
   def change
      remove_index :enrollments, :user_id
-     add_index :enrollments, [:user_id, :section_id], unique: true, name: 'index_enrollments_on_user_id_and_section_id'
+     add_index :enrollments, [ :user_id, :section_id ], unique: true, name: 'index_enrollments_on_user_id_and_section_id'
   end
 end
